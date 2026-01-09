970d6cfa168983798153b6d6f5e6f84051393596
Mihai Bocanu <mihai24vic@gmail.com>
2026-01-09 22:57:35 +0200

    Initial commit

---
+++ Masahiro/Masahiro/MasahiroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasahiroApp.swift
+//  Masahiro
+//
+//  Created by Bocanu Mihai on 09.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MasahiroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
